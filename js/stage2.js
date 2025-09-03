eeca41b37ca51e39351f995e7b572648e87f9b57
1102csh <121600882+1102csh@users.noreply.github.com>
2025-09-03 18:29:00 +0900

    Update stage2.js

--- js/stage2.js
+++ js/stage2.js
@@ -7,28 +7,27 @@ const password = 7284;
     const content = document.getElementById("content");
     const notice = document.getElementById("notice");
 
-    if (id) {
-        // 활성화
-        if (id == password) {
-            btn.style.display = 'block';
-            content.textContent = '올바른 패스워드를 입력했습니다.';
-        }
-        else if(id == "0000"){
-            content.innerText = `이전과 동일한 방식입니다.
-            올바른 패스워드를 입력하고 다음 스테이지로 넘어가세요.`;
-        }
-        else {
-            notice.textContent = '올바르지 않은 패스워드입니다.';
-        }
-
-        // 클릭/엔터로 전환
-        const go = () => location.href = './stage3.html';
-        btn.addEventListener('click', go);
-        btn.addEventListener('keydown', (e) => {
-            if (e.key === 'Enter' || e.key === ' ') {
-                e.preventDefault();
-                go();
-            }
-        });
+    // 활성화
+    if (id == password) {
+        btn.style.display = 'block';
+        content.textContent = '올바른 패스워드를 입력했습니다.';
     }
+    else if (id == "0000") {
+        content.innerText = `이전과 동일한 방식입니다.
+            올바른 패스워드를 입력하고 다음 스테이지로 넘어가세요.`;
+    }
+    else {
+        notice.textContent = '올바르지 않은 패스워드입니다.';
+    }
+
+    // 클릭/엔터로 전환
+    const go = () => location.href = './stage3.html';
+    btn.addEventListener('click', go);
+    btn.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            go();
+        }
+    });
+
 })();
